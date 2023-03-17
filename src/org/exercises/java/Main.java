a461209876dd2b39e642df10d69adeec3c040377
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-03-17 11:36:02 +0100

    add try-catch

--- src/org/exercises/java/Main.java
+++ src/org/exercises/java/Main.java
@@ -4,7 +4,13 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Event event = new Event("Opera", LocalDate.of(2023,3,22),200);
-        System.out.println(event);
+        Event event = null;
+        try {
+            event = new Event("Opera", LocalDate.of(2023,3,22),-200);
+            System.out.println(event);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
